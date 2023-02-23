3baa95181a30e75f00e69545f2ba8b6164060c43
Egle <103460996+IdiotAllex@users.noreply.github.com>
2023-02-23 19:15:40 +0200

    20 - priešo modelis, 19 - resursų modelis, 16 - resursu klase, 15 - tevine klase

---
+++ Assets/Trash/scripts/Trash.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    //value number of the resource
+    public float points;
+}
